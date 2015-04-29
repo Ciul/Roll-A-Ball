45b1c4b6b21ec8cf9b47ebbd09edc32061216194
Luis Carlos Osorio Jayk <luiscarlosjayk@gmail.com>
2015-04-29 18:09:30 -0400

    CameraController
    Added CameraController to maintain a distance from the player on each
    frame.

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	Vector3 offset;
+
+	void Start () {
+		// Calculate initial distance from player to camera.
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate () {
+		// Maintains the camera to the distance set by initial offset calculation.
+		transform.position = player.transform.position + offset;
+	}
+
+}
