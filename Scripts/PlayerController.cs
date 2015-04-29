0906270084b76c68e93c509b41759aa0644fa546
Luis Carlos Osorio Jayk <luiscarlosjayk@gmail.com>
2015-04-29 15:02:28 -0400

    Initial Commit
    The ball moves as we press the arrow keys but no physics are used. The
    ball doesn't roll at all.

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveHorizontal;
+	public float moveVertical;
+	Vector3 position;
+
+	void Start () {
+		position = transform.position;
+	}
+	
+	void Update () {
+		moveHorizontal 		= Input.GetAxis ("Horizontal");
+		moveVertical 		= Input.GetAxis ("Vertical");
+		Vector3 movement 	= new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		position = position + movement;
+		gameObject.transform.position = position;
+	}
+
+}
